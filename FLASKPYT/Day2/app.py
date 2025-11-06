6b0575f6c894379c018fd018f80ef4bab7fd6131
ABSAR <mashikabsardheen@gmail.com>
2025-11-06 19:19:12 +0530

    Added allfeedback.html and its function

--- FLASKPYT/Day2/app.py
+++ FLASKPYT/Day2/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
 import os
 from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("feedback.html")
 
-
+feedbacklist = []
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -15,7 +16,13 @@ def submit():
     feedback = request.form['feedback']
     with open('E:/PROJECTS/PYTHON/pyfullstack/FLASKPYT/Day2/templates/trash', 'a') as trashadd:
         trashadd.write(f"Name : {name}\nFeedback: {feedback}\nTime: {datetime.now()} \n{'-'*40}\n")
+    feedbacklist.append({'name': name, 'fb': feedback})
     return render_template("thankyou.html", name=name, feedback=feedback)
+
+@app.route('/allfeedbacks')
+def collectfeedbacks():
+    return render_template("allfeedbacks.html", all=feedbacklist)
+    
 
 
 if __name__=="__main__":
