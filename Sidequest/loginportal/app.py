849a52154645e0552b481dc8def83dd58f1c67d5
ABSAR <mashikabsardheen@gmail.com>
2025-11-13 22:18:10 +0530

    Created Register Feature, also redirects to /login

--- Sidequest/loginportal/app.py
+++ Sidequest/loginportal/app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 
 
 app = Flask(__name__)
 
 global_validcreds = {'adam':'adam', 'bison':'unsung', 'admin':'admin'}
 
+@app.route('/')
+def home():
+    return render_template("index.html")
 
+#LOGIN PAGE
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method ==  'POST':
@@ -21,6 +25,19 @@ def login():
         else:
             return f"User Not Found"
     return render_template("login.html")
+
+
+#REGISTER PAGE
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        global_validcreds[user] = password
+        print(global_validcreds)
+        return redirect("/login")
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
